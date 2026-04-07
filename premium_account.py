dc1c6b059e7f4b6691302400530a1b49279cbe2c
qv1t <127725343+4sqc4@users.noreply.github.com>
2026-04-07 13:06:28 +0300

    Update premium_account.py

--- premium_account.py
+++ premium_account.py
@@ -5,11 +5,11 @@ class PremiumAccount(BankAccount):
     def __init__(self, owner_name, balance, overdraft_limit=50000):
         super().__init__(owner_name, balance)
         self.overdraft_limit = overdraft_limit
-        self.fee = 500 # Фиксированная комиссия за обслуживание
+        self.fee = 500
 
     def withdraw(self, amount):
         self._check_status()
-        # Лимит = текущий баланс + допустимый минус
+
         if amount > (self._balance + self.overdraft_limit):
             raise InsufficientFundsError("Превышен лимит овердрафта")
         self._balance -= amount
